256411cf27bd2211c243da3d101cedc1f366c0fd
Li, Chenglu <kabelee92@gmail.com>
2019-11-06 15:52:36 -0600

    Refactor interact.py to module

--- interact.py
+++ interact.py
@@ -64,25 +64,26 @@ def sample_sequence(personality, history, tokenizer, model, args, current_output
     if current_output is None:
         current_output = []
 
-    for i in range(args.max_length):
+    for i in range(args["max_length"]):
         instance = build_input_from_segments(
             personality, history, current_output, tokenizer, with_eos=False)
 
         input_ids = torch.tensor(
-            instance["input_ids"], device=args.device).unsqueeze(0)
+            instance["input_ids"], device=args["device"]).unsqueeze(0)
         token_type_ids = torch.tensor(
-            instance["token_type_ids"], device=args.device).unsqueeze(0)
+            instance["token_type_ids"], device=args["device"]).unsqueeze(0)
 
         logits = model(input_ids, token_type_ids=token_type_ids)
         if isinstance(logits, tuple):  # for gpt2 and maybe others
             logits = logits[0]
-        logits = logits[0, -1, :] / args.temperature
-        logits = top_filtering(logits, top_k=args.top_k, top_p=args.top_p)
+        logits = logits[0, -1, :] / args["temperature"]
+        logits = top_filtering(
+            logits, top_k=args["top_k"], top_p=args["top_p"])
         probs = F.softmax(logits, dim=-1)
 
         prev = torch.topk(probs, 1)[
-            1] if args.no_sample else torch.multinomial(probs, 1)
-        if i < args.min_length and prev.item() in special_tokens_ids:
+            1] if args["no_sample"] else torch.multinomial(probs, 1)
+        if i < args["min_length"] and prev.item() in special_tokens_ids:
             while prev.item() in special_tokens_ids:
                 if probs.max().item() == 1:
                     warnings.warn(
@@ -97,61 +98,48 @@ def sample_sequence(personality, history, tokenizer, model, args, current_output
     return current_output
 
 
-def run():
-    parser = ArgumentParser()
-    parser.add_argument("--dataset_path", type=str, default="",
-                        help="Path or url of the dataset. If empty download from S3.")
-    parser.add_argument("--dataset_cache", type=str,
-                        default='./dataset_cache', help="Path or url of the dataset cache")
-    parser.add_argument("--model", type=str, default="openai-gpt", help="Model type (openai-gpt or gpt2)",
-                        choices=['openai-gpt', 'gpt2'])  # anything besides gpt2 will load openai-gpt
-    parser.add_argument("--model_checkpoint", type=str,
-                        default="", help="Path, url or short name of the model")
-    parser.add_argument("--max_history", type=int, default=2,
-                        help="Number of previous utterances to keep in history")
-    parser.add_argument("--device", type=str, default="cuda" if torch.cuda.is_available()
-                        else "cpu", help="Device (cuda or cpu)")
-
-    parser.add_argument("--no_sample", action='store_true',
-                        help="Set to use greedy decoding instead of sampling")
-    parser.add_argument("--max_length", type=int, default=20,
-                        help="Maximum length of the output utterances")
-    parser.add_argument("--min_length", type=int, default=1,
-                        help="Minimum length of the output utterances")
-    parser.add_argument("--seed", type=int, default=42, help="Seed")
-    parser.add_argument("--temperature", type=int, default=0.7,
-                        help="Sampling softmax temperature")
-    parser.add_argument("--top_k", type=int, default=0,
-                        help="Filter top-k tokens before sampling (<=0: no filtering)")
-    parser.add_argument("--top_p", type=float, default=0.9,
-                        help="Nucleus filtering (top-p) before sampling (<=0.0: no filtering)")
-    args = parser.parse_args()
+def answer(
+    dataset_path,
+    dataset_cache='dataset_cache',
+    model='gpt2',
+    model_checkpoint='',
+    max_history=1,
+    device="cuda" if torch.cuda.is_available() else "cpu",
+    no_sample=True,
+    max_length=20,
+    min_length=1,
+    seed=42,
+    temperature=0.7,
+    top_k=0,
+    top_p=0.9
+):
+    args = locals()
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__file__)
     logger.info(pformat(args))
 
-    if args.model_checkpoint == "":
-        if args.model == 'gpt2':
+    if model_checkpoint == "":
+        if model == 'gpt2':
             raise ValueError(
                 "Interacting with GPT2 requires passing a finetuned model_checkpoint")
         else:
-            args.model_checkpoint = download_pretrained_model()
+            model_checkpoint = download_pretrained_model()
 
-    random.seed(args.seed)
-    torch.random.manual_seed(args.seed)
-    torch.cuda.manual_seed(args.seed)
+    random.seed(seed)
+    torch.random.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
 
     logger.info("Get pretrained model and tokenizer")
-    tokenizer_class, model_class = (GPT2Tokenizer, GPT2LMHeadModel) if args.model == 'gpt2' else (
+    tokenizer_class, model_class = (GPT2Tokenizer, GPT2LMHeadModel) if model == 'gpt2' else (
         OpenAIGPTTokenizer, OpenAIGPTLMHeadModel)
-    tokenizer = tokenizer_class.from_pretrained(args.model_checkpoint)
-    model = model_class.from_pretrained(args.model_checkpoint)
-    model.to(args.device)
+    tokenizer = tokenizer_class.from_pretrained(model_checkpoint)
+    model = model_class.from_pretrained(model_checkpoint)
+    model.to(device)
     add_special_tokens_(model, tokenizer)
 
     logger.info("Sample a personality")
-    dataset = get_dataset(tokenizer, args.dataset_path, args.dataset_cache)
+    dataset = get_dataset(tokenizer, dataset_path, dataset_cache)
     personalities = [dialog["personality"]
                      for dataset in dataset.values() for dialog in dataset]
     personality = random.choice(personalities)
@@ -169,10 +157,6 @@ def run():
             out_ids = sample_sequence(
                 personality, history, tokenizer, model, args)
         history.append(out_ids)
-        history = history[-(2*args.max_history+1):]
+        history = history[-(2*max_history+1):]
         out_text = tokenizer.decode(out_ids, skip_special_tokens=True)
         print(out_text)
-
-
-if __name__ == "__main__":
-    run()
